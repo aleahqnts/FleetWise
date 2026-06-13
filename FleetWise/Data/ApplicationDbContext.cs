9177c6c46a50cbdeb361d3cd2491454d88a119ae
Franz Kester P. Calumpit <98814767+franskeiser@users.noreply.github.com>
2026-06-14 03:26:23 +0800

    Remove dead SQL Server/ASP.NET Identity stack

--- FleetWise/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace FleetWise.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
-    {
-    }
-}
