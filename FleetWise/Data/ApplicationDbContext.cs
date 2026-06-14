62376cd1d96ef3afe6f7ac57d50fb92c636416c0
Franz Kester P. Calumpit <98814767+franskeiser@users.noreply.github.com>
2026-06-14 12:04:55 +0800

    Revert "Remove dead SQL Server/ASP.NET Identity stack"
    This reverts commit 9177c6c46a50cbdeb361d3cd2491454d88a119ae.

---
+++ FleetWise/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FleetWise.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
